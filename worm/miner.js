c07edadc8773f144aa796cee33e49b139bff1b2b
Arthur Freitas <bfreitas.arthur@gmail.com>
2022-06-10 22:19:58 -0300

    Remove miner log messages

--- worm/miner.js
+++ worm/miner.js
@@ -8,17 +8,11 @@ export async function main(ns) {
 		var currentSecurity = ns.getServerSecurityLevel(target);
 		var currentMoney = ns.getServerMoneyAvailable(target);
 
-        var weakenIterationsLeft = Math.ceil((currentSecurity - securityThresh) / 0.05)
-        var growthIterationsLeft = Math.ceil((moneyThresh - currentMoney) / ns.getServerGrowth(target));
-
         if (currentSecurity > securityThresh) {
-			ns.print("Weaking: " + weakenIterationsLeft + " iterations left ")
             await ns.weaken(target);
         } else if (currentMoney < moneyThresh) {
-			ns.print("Growing: " + growthIterationsLeft + " iterations left ")
             await ns.grow(target);
         } else {
-			ns.print("Hacking for " + ns.getHackTime())
             await ns.hack(target);
         }
     }
